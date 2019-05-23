2512f74ef03d4727d1fc5eb8bb6af137ecfa3ff6
andreacb1995 <andrea.carballido.ballesteros@gmail.com>
2019-05-23 17:52:04 +0200

    Get id

--- server.js
+++ server.js
@@ -1,18 +1,39 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var usuariosOnline = [];
+var mysql = require('mysql2');
 
 
 var app = express();
+var fs = require('fs');
+
 app.use(bodyParser.json());
 
 app.use(express.static('www'));
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
-app.get('/', (req, res) => {
-    res.send("Hola mundo");
-})
+
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "pokemon",
+    password: "Aula@2019",
+    database: "pokestudio",
+    port: 3306
+
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+
+
+// app.get('/', (req, res) => {
+//     res.send("Hola mundo");
+// });
 
 app.post('/', (req, res) => {
     let nombre = req.body.nombre
@@ -32,6 +53,24 @@ app.post('/login', (req, res) => {
         res.send({ "result": "usuario o contraseña incorrecto" });
     }
 });
+
+
+//?id=1
+app.get('/', (req, res) => {
+    let id = req.query.id;
+    console.log(id);
+    con.query(
+        `SELECT * FROM JUGADOR where id_jugador=${id}`,
+        function (err, results) {
+            console.log(results);
+            let data = {
+                "result": results
+            }
+            res.send(data);
+        }
+    );
+});
+
 
 
 let puerto = 4000;
