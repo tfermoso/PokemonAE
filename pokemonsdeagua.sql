df5beaff974d1d721efeadbb666be8f94bbf473f
EMILIO GONZÁLEZ GONZÁLEZ <EMILIO.GONZLEZ@aula.local>
2019-06-06 16:13:31 +0200

    sql cambiado

--- pokemonsdeagua.sql
+++ pokemonsdeagua.sql
@@ -1,403 +1,164 @@
-insert into tipos (id. nombre, fuerte_cotra, debil_contra) values (6, 'Agua', 3, 4);
+insert into tipos (id. nombre, fuerte_cotra, debil_contra) (6, 'Agua', 3, 4);
 
-
+insert into pokemon_base (nombre, tipo, vida, ar1, ar2, ac1, ac2, carga1, carga2) 
+values
 -- 1)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Azumarill', 6, 250, 15, 12, 130, 90, 10, 5);
 -- 2)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Barboach', 6, 220, 5, 5, 50, 90, 3, 5);
 -- 3)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Blastoise', 6, 300, 6, 5, 130, 100, 10, 10);
 -- 4)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Buizel', 6, 230, 5, 8, 70, 60, 5, 5);
 -- 5)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Carvanha', 6, 250, 6, 12, 45, 70, 3, 3);
 -- 6)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Squirtle', 6, 220, 5, 12, 45, 70, 3, 5);
 -- 7)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Wartortle', 6, 250, 6, 5, 45, 90, 3, 5);
 -- 8)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Psyduck', 6, 210, 12, 5, 50, 70, 3, 5);
 -- 9)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Golduck', 6, 250, 20, 5, 90, 100, 5, 10);
 -- 10)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Poliwag', 6, 220, 12, 5, 55, 50, 3, 3);
 -- 11)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Poliwhirl', 6, 260, 12, 15, 90, 50, 5, 3);
 -- 12)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Poliwrath', 6, 290, 12, 15, 90, 50, 5, 3);
 -- 13)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Tentacool', 6, 230, 12, 5, 70, 60, 5, 3);
 -- 14)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Tentacruel', 6, 260, 9, 10, 130, 110, 10, 10);
 -- 15)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Slowpoke', 6, 240, 5, 20, 100, 70, 10, 5);
 -- 16)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Slowbro', 6, 270, 5, 20, 100, 70, 10, 5);
 -- 17)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Seel', 6, 230, 12, 5, 80, 60, 5, 3);
 -- 18)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Dewgong', 6, 260, 10, 15, 80, 130, 5, 10);
 -- 19)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Shellder', 6, 240, 12, 5, 70, 60, 5, 3);
 -- 20)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Krabby', 6, 235, 12, 5, 70, 45, 5, 3);
 -- 21)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Kingler', 6, 255, 12, 8, 70, 45, 5, 3);
 -- 22)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Horsea', 6, 220, 5, 12, 90, 100, 5, 10);
 -- 23)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Seadra', 6, 250, 5, 6, 90, 80, 5, 5);
 -- 24)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Goldeen', 6, 220, 5, 10, 50, 70, 3, 5);
 -- 25)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Seaking', 6, 260, 16, 10, 90, 90, 5, 10);
 -- 26)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Staryu', 6, 230, 5, 5, 80, 45, 5, 3);
 -- 27)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Starmie', 6, 270, 15, 5, 100, 90, 10, 5);
 -- 28)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Magikarp', 6, 200, 0, 0, 35, 35, 0, 0);
 -- 29)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Gyarados', 6, 290, 6, 16, 130, 110, 10, 5);
 -- 30)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Lapras', 6, 260, 10, 5, 130, 65, 10, 5);
 -- 31)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Vaporeon', 6, 240, 5, 5, 130, 50, 10, 3);
 -- 32)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Totodile', 6, 230, 5, 6, 70, 50, 5, 3);
 -- 33)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Croconaw', 6, 260, 5, 6, 70, 50, 5, 3);
 -- 34)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Feraligart', 6, 280, 16, 6, 130, 90, 10, 5);
 -- 35)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Chinchou', 6, 220, 5, 8, 80, 45, 5, 3);
 -- 36)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Lanturn', 6, 250, 5, 8, 130, 80, 10, 5);
 -- 37)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Marill', 6, 240, 5, 12, 50, 45, 3, 3);
 -- 38)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Wooper', 6, 235, 5, 5, 100, 55, 5, 3);
 -- 39)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Quagsire', 6, 265, 5, 5, 120, 80, 10, 5);
 -- 40)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Slowking', 6, 270, 5, 20, 140, 100, 10, 5);
 -- 41)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Qwilfish', 6, 250, 5, 5, 110, 90, 10, 5);
 -- 42)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Corsola', 6, 240, 5, 12, 80, 50, 5, 3);
 -- 43)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Remoraid', 6, 245, 5, 5, 80, 50, 5, 3);
 -- 44)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Octillery', 6, 265, 5, 5, 130, 80, 10, 5);
 -- 45)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Mantine', 6, 260, 12, 8, 90, 55, 10, 3);
 -- 46)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Kingdra', 6, 230, 16, 6, 130, 110, 10, 5);
 -- 47)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Suicune', 6, 260, 12, 12, 90, 130, 5, 10);
 -- 48)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Mudkip', 6, 230, 5, 5, 100, 55, 5, 3);
 -- 49)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Marshtomp', 6, 250, 5, 5, 65, 50, 5, 3);
 -- 50)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Swampert', 6, 280, 5, 5, 120, 65, 10, 5);
 -- 51)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Lotad', 6, 230, 5, 13, 45, 90, 3, 5);
 -- 52)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Lombre', 6, 270, 12, 13, 90, 90, 5, 5);
 -- 53)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Ludicolo', 6, 280, 12, 13, 180, 90, 10, 5);
 -- 54)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Wingull', 6, 230, 8, 5, 90, 60, 5, 3);
 -- 55)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Pelipper', 6, 250, 8, 5, 130, 110, 10, 10);
 -- 56)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Sharpedo', 6, 260, 6, 16, 130, 70, 10, 5);
 -- 57)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Wailmer', 6, 285, 0, 5, 70, 50, 5, 3);
 -- 58)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Wailord', 6, 300, 12, 5, 150, 65, 10, 5);
 -- 59)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Whiscash', 6, 280, 5, 5, 130, 70, 10, 5);
 -- 60)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Corphish', 6, 260, 12, 15, 45, 45, 3, 3);
 -- 61)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Crawdaunt', 6, 280, 16, 12, 50, 45, 5, 3);
 -- 62)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Feebas', 6, 230, 0, 5, 60, 60, 3, 3);
 -- 63)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Milotic', 6, 270, 16, 15, 150, 65, 10, 5);
 -- 64)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Castform', 6, 230, 5, 5, 130, 90, 10, 5);
 -- 65)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Clamperi', 6, 250, 5, 5, 90, 50, 5, 3);
 -- 66)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Huntail', 6, 230, 5, 6, 90, 50, 5, 3);
 -- 67)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Gorebyss', 6, 255, 5, 20, 100, 60, 10, 5);
 -- 68)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Relicanth', 6, 270, 5, 12, 130, 70, 10, 5);
 -- 69)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Luvdisc', 6, 230, 5, 0, 70, 45, 5, 3);
 -- 70)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Kyogre', 6, 280, 16, 16, 130, 130, 10, 10);
 -- 71)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Piplup', 6, 210, 7, 12, 60, 60, 5, 5);
 -- 72)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Prinplup', 6, 250, 8, 12, 130, 60, 10, 5);
 -- 73)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Empoleon', 6, 280, 8, 12, 130, 60, 10, 5);
 -- 74)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Flotazel', 6, 260, 5, 16, 130, 60, 10, 5);
 -- 75)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Shellos', 6, 210, 15, 15, 70, 55, 5, 3);
 -- 76)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Gastrodon', 6, 250, 15, 15, 120, 70, 10, 5);
 -- 77)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Finneon', 6, 230, 7, 5, 90, 70, 5, 5);
 -- 78)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Lumineon', 6, 260, 16, 5, 130, 70, 10, 5);
 -- 79)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Mantyke', 6, 280, 5, 12, 90, 70, 5, 5);
 -- 80)
-insert into pokemon_base 
-(nombre, tipo, salud_max, ar1, ar2, ac1, ac2, carga1, carga2) 
-values 
 ('Palkia', 6, 280, 15, 6, 150, 100, 10, 5);
