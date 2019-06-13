fb2b2b88caa7734e31caea2794e792225ce83f80
juaigl <jmif@outlook.com>
2019-06-13 18:30:14 +0200

    update map in js and html

--- www/Mapa/index.js
+++ www/Mapa/index.js
@@ -9,16 +9,22 @@ $('document').ready(() => {
         usrOnline.append(p);
     });
 
-    let map = [[]]; ///mapa de mario y andrea
+    let map = [["1", "2", "3", "4", "5", "6"], ["1", "2", "3", "4", "5", "6"],
+    ["1", "2", "3", "4", "5", "6"], ["1", "2", "3", "4", "5", "6"],
+    ["1", "2", "3", "4", "5", "6"], ["1", "2", "3", "4", "5", "6"]]; ///mapa de mario y andrea
     let htmlmap = $('#map');
-    for (let f of map){
+    for (let f of map) {
         let hoz = document.createElement("div");
-        for (let c of map){
+        for (let c of f) {
             let div = document.createElement("div");
             div.innerHTML = c;
+            div.classList.add("col-sm-2");
+            div.classList.add("justify-content-center");
+            hoz.classList.add("tex-center");
             hoz.append(div);
         }
-        
+        hoz.classList.add("row");
+        htmlmap.append(hoz);
     }
 });
 
