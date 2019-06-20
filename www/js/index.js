b5320f9888a8ad0721220d0e3464abc0f62c62b8
Tomás Fermoso Dominguez <tomas@aula.local>
2019-06-20 16:26:46 +0200

    añadida pagina de inicio juego

--- www/js/index.js
+++ www/js/index.js
@@ -14,6 +14,31 @@ $('document').ready(() => {
             }
         })
      }, 3000);
-    
+     let users = ["aaaa", "bbbbb"]; //usuarios online base datos
+     let usrOnline = $('#users-list');
+     usrOnline.html("");
+     users.forEach(element => {
+         let p = document.createElement("p");
+         p.innerHTML = element
+         usrOnline.append(p);
+     });
+ 
+     let map = [["1", "2", "3", "4", "5", "6"], ["1", "2", "3", "4", "5", "6"],
+     ["1", "2", "3", "4", "5", "6"], ["1", "2", "3", "4", "5", "6"],
+     ["1", "2", "3", "4", "5", "6"], ["1", "2", "3", "4", "5", "6"]]; ///mapa de mario y andrea
+     let htmlmap = $('#map');
+     for (let f of map) {
+         let hoz = document.createElement("div");
+         for (let c of f) {
+             let div = document.createElement("div");
+             div.innerHTML = c;
+             div.classList.add("col-sm-2");
+             div.classList.add("justify-content-center");
+             hoz.classList.add("tex-center");
+             hoz.append(div);
+         }
+         hoz.classList.add("row");
+         htmlmap.append(hoz);
+     } 
  
 })
